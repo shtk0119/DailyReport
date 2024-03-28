899c0f1de432b4177c39066ed174b9d2f10237c6
kaxia7216 <kaxia.khlove@gmail.com>
2024-03-28 20:14:15 +0900

    ログインページの作成

--- client/app/(auth)/login/page.tsx
+++ client/app/(auth)/login/page.tsx
@@ -1,10 +1,23 @@
+'use client';
 import Link from 'next/link';
 import React, { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { PasswordShowIcon } from '../../_components/passwordShowIcon';
+import { PasswordHideIcon } from '../../_components/passwordHideIcon';
 
 const Login = () => {
+  const router = useRouter();
+  const [showPassword, setShowPassword] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
   const startLogin = () => {
+      //認証用APIに接続
+      //成功の場合、HOME画面に遷移
+  };
 
+  const pushedShowIcon = () => {
+    setShowPassword(!showPassword);
   };
 
   return (
@@ -16,23 +29,38 @@ const Login = () => {
             <fieldset>
               <fieldset className='pb-5'>
                 <p className='text-[#B5BAC1] text-xs pb-1'>メールアドレス</p>
-                <input type="text" className='bg-[#1E1F22] w-full h-[42px] rounded-lg' />
+                <input
+                 type="text"
+                 className='bg-[#1E1F22] w-full h-[42px] rounded-lg px-2'
+                 onChange={(e) => {setEmail(e.target.value)}}
+                />
               </fieldset>
-              <fieldset className='pb-5'>
+              <fieldset className='pb-5 relative'>
                 <p className='text-[#B5BAC1] text-xs pb-1'>パスワード</p>
-                <input type="text" className='bg-[#1E1F22] w-full h-[42px] rounded-lg' />
+                <input
+                 type={`${showPassword? 'text' : 'password'}`}
+                 className='bg-[#1E1F22] w-full h-[42px] rounded-lg px-2'
+                 onChange={(e) => {setPassword(e.target.value)}}
+                />
+                <button onClick={pushedShowIcon} type='button' className='absolute inset-y-0 right-0 flex items-center pr-3 pb-5'>
+                  {showPassword? <PasswordHideIcon className='size-5' /> :<PasswordShowIcon className='size-5' /> }
+                </button>
                 <div className='flex justify-end'>
                   <Link href="#" className='text-[#1E90FF]'>パスワードをお忘れですか？</Link>
                 </div>
               </fieldset>
               <fieldset>
-                <button type="button" className='tracking-tighter bg-[#5865F2] w-full h-[45px] text-2xl font-mono rounded-lg'>
+                <button
+                 type="button"
+                 className='tracking-tighter bg-[#5865F2] w-full h-[45px] text-2xl font-mono rounded-lg'
+                 onClick={startLogin}
+                >
                   ログイン
                 </button>
               </fieldset>
               <fieldset className='pt-4 pb-2'>
                 <input type="radio" />
-                <span className='text-sm'>  次回から自動でログイン</span>
+                <span className='text-sm'> 次回から自動でログイン</span>
               </fieldset>
               <fieldset>
                 <Link href="/register" className='text-[#1E90FF] text-sm'>登録がまだの方はこちら</Link>
