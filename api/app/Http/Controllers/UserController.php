6be8b04bbb55a25457a8ff2c01d93e3d5e6976ff
shtk0119 <shtk0119@icloud.com>
2024-03-29 19:24:41 +0900

    ディレクトリ 変更

--- api/app/Http/Controllers/UserController.php
+++ api/app/Http/Controllers/UserController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
-use App\Http\Resources\UserCollection;
+use App\Http\Resources\User\UserCollection;
 
 class UserController extends Controller
 {
