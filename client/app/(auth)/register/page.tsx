f3d68f0183ecbcfe3c38b0c4cfb9a41131f44f78
kaxia7216 <kaxia.khlove@gmail.com>
2024-03-30 06:07:56 +0900

    アカウント新規登録APIの紐づけ

--- client/app/(auth)/register/page.tsx
+++ client/app/(auth)/register/page.tsx
@@ -1,25 +1,53 @@
 'use client';
 import Link from 'next/link';
+import axios from 'axios';
 import React, { useState } from 'react';
+import { useRouter } from 'next/navigation';
 import { PasswordShowIcon } from '../../_components/passwordShowIcon';
 import { PasswordHideIcon } from '../../_components/passwordHideIcon';
 
 const Register = () => {
+  const router = useRouter();
   const [showPassword, setShowPassword] = useState(false);
-  const [userName, setUserName] = useState('');
-  const [groupNumber, setGroupNumber] = useState('');
+  const [name, setName] = useState('');
+  const [group, setGroup] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [checkPassword, setCheckPassword] = useState('');
 
-  const startRegister = () => {
-    //認証用APIに接続
-    //成功の場合、HOME画面に遷移
-};
+  const userRegister = async () => {
+    if(password !== checkPassword){
+      return alert('パスワードが一致していません、再入力してください');
+    }
 
-const pushedShowIcon = () => {
-  setShowPassword(!showPassword);
-};
+    //登録するデータを作成
+    const newUser = { user: { name, email, password, group } };
+    const loginUser = { user: { email, password }};
+
+    //新規ユーザ認証
+    try {
+      const resRegister = await axios.post('http://localhost/api/register', newUser);
+    } catch(error) {
+      throw new Error('アカウント登録できませんでした');
+    }
+
+    //ログイン認証
+    try {
+      const resLogin = await axios.post('http://localhost/api/login', loginUser);
+      const tokenInfo = resLogin.data['access_token'];
+      const setToken = tokenInfo.split('|');
+      sessionStorage.setItem('authToken', setToken[1]);
+    } catch (error) {
+      throw new Error('認証失敗しました');
+    }
+
+    //成功の場合、HOME画面に遷移
+    router.push('/home');
+  };
+
+  const pushedShowIcon = () => {
+    setShowPassword(!showPassword);
+  };
 
   return (
     <div className='flex justify-center items-center min-h-screen'>
@@ -32,7 +60,7 @@ const pushedShowIcon = () => {
               <input
                 type="text"
                 className='bg-[#1E1F22] w-full h-[42px] rounded-lg px-2'
-                onChange={(e) => {setUserName(e.target.value)}}
+                onChange={(e) => {setName(e.target.value)}}
               />
             </fieldset>
             <fieldset className='pb-4 md:pb-5'>
@@ -40,7 +68,7 @@ const pushedShowIcon = () => {
               <input
                 type="text"
                 className='bg-[#1E1F22] w-full h-[42px] rounded-lg px-2'
-                onChange={(e) => {setGroupNumber(e.target.value)}}
+                onChange={(e) => {setGroup(e.target.value)}}
               />
             </fieldset>
             <fieldset className='pb-4 md:pb-5'>
@@ -77,7 +105,7 @@ const pushedShowIcon = () => {
               <button
                 type="button"
                 className='tracking-tighter bg-[#5865F2] w-full h-[45px] text-2xl font-mono rounded-lg'
-                onClick={startRegister}
+                onClick={userRegister}
               >
                 作成する
               </button>
