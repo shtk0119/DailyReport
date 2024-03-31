27c7b8a94a0425ac959874dbc3e46f8d3ddeb805
shtk0119 <shtk0119@icloud.com>
2024-03-31 09:13:47 +0900

    home fix

--- client/app/(private)/_components/Header.tsx
+++ client/app/(private)/_components/Header.tsx
@@ -2,8 +2,8 @@ import { GearIcon } from "./GearIcon";
 
 const Header = () => {
   return (
-    <div className="text-white bg-main flex items-center justify-between py-6">
-      <h1 className="text-2xl ml-8 md:ml-8 lg:ml-16">デイリポ＋</h1>
+    <div className="text-white bg-main flex items-center justify-between py-4">
+      <h1 className="text-2xl font-bold ml-8 md:ml-8 lg:ml-16">デイリポ＋</h1>
       <div className="flex">
         <GearIcon />
         <h1 className="text-2xl mr-8 md:mr-8 lg:mr-16">User1</h1>
